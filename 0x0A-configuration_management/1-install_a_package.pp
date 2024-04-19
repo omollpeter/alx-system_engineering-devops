2add71a368e0bb6dcc918d6401cf85e04b212bd2
Peter Omollo <omollpeter@gmail.com>
2024-04-19 20:27:29 +0300

    Fix 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 #This manifest install a package flask from pip3
+package { 'pip3':
+    ensure   => 'installed'
+}
+
 package { 'flask':
     ensure   => '2.1.0',
-    provider => 'pip3'
+    provider => 'pip3',
+    require  => Package['pip3']
 }
