f2efbb0be04d5063912239933969be7ce07c7d23
Peter Omollo <omollpeter@gmail.com>
2024-06-13 17:49:25 +0300

    Fix puppet manifest

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,12 @@
 # This puppet manifest increases the max open files limit by editing /etc/default/nginx
 
+service { 'nginx':
+    ensure => 'running',
+    enable => true
+}
+
 exec { 'edit-default-nginx':
     command => "sed -i 's/-n 15/-n 4096/' /etc/default/nginx",
-    path    => '/bin'
+    path    => '/bin',
+    notify  => Service['nginx']
 }
